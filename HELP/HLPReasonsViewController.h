6a1aa85c21d3980b834c0ca635dc69e27b28cbed
Vladislav Mazur <vlad@mazuru.com>
2014-05-18 11:25:00 +0300

    new creens

---
+++ HELP/HLPReasonsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HLPReasonsViewController.h
+//  HELP
+//
+//  Created by Vladislav on 5/18/14.
+//  Copyright (c) 2014 WildSpirit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLPReasonsViewController : UITableViewController
+
+@end
